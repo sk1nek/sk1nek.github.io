467762b589d2dba0e932534f0ce97b3955468170
Michal <238181@student.pwr.edu.pl>
2018-02-27 22:21:46 +0100

    Working email submission

--- js/js.js
+++ js/js.js
@@ -9,3 +9,16 @@ function hideContactForm(){
     $('#contact-form-holder').hide();
     $('#initial').show();
 }
+
+function sendMail() {
+
+    var title = encodeURIComponent($('#title').val());
+    var msg = encodeURIComponent($('#msg').val());
+
+    console.log(title);
+    console.log(msg);
+
+    var mailTo = "mailto:me@mjaroszewicz.me?subject=" + title  + "&body=" + msg
+    window.location = mailTo;
+
+}
