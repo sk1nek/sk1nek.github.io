057a88fc2e5dc9f135096860d63ca61f6435f6eb
Michal <238181@student.pwr.edu.pl>
2018-02-27 21:53:18 +0100

    Added contact form

---
+++ js/js.js
@@ -0,0 +1,11 @@
+
+
+function showContactForm() {
+    $('#initial').hide();
+    $('#contact-form-holder').show();
+}
+
+function hideContactForm(){
+    $('#contact-form-holder').hide();
+    $('#initial').show();
+}
